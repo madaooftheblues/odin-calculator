7298d62c418ffebbf293a0b1196f73eef53a661a
madaooftheblues <azlanrasool@gmail.com>
2022-08-05 00:17:31 +0500

    Add basic style

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const digits = document.querySelectorAll(".digit");
+const display = document.querySelector(".display");
+const operators = document.querySelectorAll(".operator");
+const equals = document.getElementById("equals");
+const clear = document.getElementById("clear");
+
+let numberA = null;
+let numberB = null;
+let operator = null;
+
 function add(a, b) {
   return a + b;
 }
@@ -15,5 +25,17 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-  return operator(a, b);
+  a = parseInt(a);
+  b = parseInt(b);
+  if (operator == "+") return add(a, b);
+  if (operator == "-") return subtract(a, b);
+  if (operator == "*") return multiply(a, b);
+  if (operator == "/") return divide(a, b);
+}
+
+function populate(e) {
+  if (state === 1) display.textContent = "";
+  display.textContent =
+    display.textContent == 0 ? e.target.id : display.textContent + e.target.id;
+  state = 0;
 }
